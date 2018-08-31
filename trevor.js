48108788602aa57a9229fe3d8109c6811e687178
sullyrox <36315158+sullyrox@users.noreply.github.com>
2018-08-30 19:07:04 -0700

    v0.82b2

--- trevor.js
+++ trevor.js
@@ -1,11 +1,14 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const os = require('os');
 //const cpuStat = require('cpu-stat');
 const client = new Discord.Client();
 client.login('YOUR_TOKEN_HERE');
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`)
+});
+client.on('ready', async () => {
+  client.user.setActivity("with your blankets | !help"); 
 });
 function B2GB(n) {
 	return n/1073741824;
@@ -136,6 +139,14 @@ if (msg.content.toLowerCase() === '!asktrev what are your relationship interests
     msg.channel.send('Blankets!');}
 if (msg.content.toLowerCase() === '!asktrev are you the best bot?') {
     msg.channel.send('Mee6 and Tatsumaki can succ me, I am the best bot!');}
+if (msg.content.toLowerCase() === '!asktrev is the pool clean?') {
+    msg.channel.send('You tell me! **throws camera at pool**');}
+if (msg.content.toLowerCase() === '!asktrev what cat type is trev?' | msg.content.toLowerCase() === '!asktrev what cat is trev?' | msg.content.toLowerCase() === '!asktrev what cat type is trevor?' | msg.content.toLowerCase() === '!asktrev what cat is trevor?') {
+    msg.channel.send('Manx, he has tabby markings.');}
+if (msg.content.toLowerCase() === '!asktrev will you start a youtube channel?' | msg.content.toLowerCase() === '!asktrev will you start a youtube channel?') {
+    msg.channel.send('Probably not, but my master has made a YouTube channel. youtube.com/sullyrox');}
+if (msg.content.toLowerCase() === '!asktrev are you the best cat?' | msg.content.toLowerCase() === '!asktrev are you the best tomcat?' | msg.content.toLowerCase() === '!asktrev are you the best feline?') {
+    msg.channel.send('Yes, all other cats can succ me!');}
   if (catfood(msg.content.toLowerCase())) { 
 	console.log(':trevert:');
 	msg.channel.send('My favorite cat food is Fancy Feast <:trev:429029722001899520>');
@@ -150,8 +161,28 @@ if (msg.content.toLowerCase() === '!piggy') {
 	.setImage("https://cdn.discordapp.com/attachments/220651459237249024/460332881567285248/Apple-iPhoneX-Silver-1-3x.jpg");
 	msg.channel.send(piggy);
 }
+if (msg.content.toLowerCase() === '!ew1' | msg.content.toLowerCase() === '!idku') {
+	var ew1 = new Discord.RichEmbed()
+	.setAuthor("Trevor", client.user.avatarURL)
+	.setTitle("NO, THE IDKU WAS WINDOWS 10 RTM!")
+	.setImage("https://cdn.discordapp.com/attachments/459260749479411713/477026060488278036/10240.png");
+	msg.channel.send(ew1);
+}
+if (msg.content.toLowerCase() === '!credits') {
+	var rembed = new Discord.RichEmbed()
+	.setTitle("Bot Credits:")
+	.setAuthor("Trevor", client.user.avatarURL)
+	.setColor(3447003)
+	.addBlankField(true)
+	.setDescription("")
+	.addField("Idea Creators", "ZmeyKolbasnik#2717\nsullyrox#0001")
+	.addField("Coders", "ZmeyKolbasnik#2717\nEpicness#8422\nsullyrox#0001\nDéžo#7209")
+	msg.channel.send(rembed);
+}
+if (msg.content.toLowerCase() === 't!credits <@281959072142458881> 250') {
+    msg.channel.send('Thanks for your purchase! If a discount applies to you, you will be paid back!');}
   if (msg.content.toLowerCase() === '!help') {
-    msg.channel.send('Commands: \n!roast: Best roasts from Trevor! \n!asktrev <question>: Trevor will respond to your question (E.G: "How much does Trevor weigh?", "When was Trevor born?") \n!pyramid <number>: COMING SOON: Makes an emoji pyramid, the maximum amount of emojis will be the number you specified. Max number allowed: 27 \n!say <phrase>: Says what you typed! \n!catpicoftheday: BETA: Triggers a random Trevor pic\n!mindbleach/!eyebleach: Shows you a calming/cute photo (of Trevor).\n!howtrueis: How true is the info you requested?\n!tehurn/!nottehurn: tehurn.com and tehurn.com/not\n!info: Detailed information about the bot\'s hosting PC');}
+    msg.channel.send('Commands: \n!roast: Best roasts from Trevor! \n!asktrev <question>: Trevor will respond to your question (E.G: "How much does Trevor weigh?", "When was Trevor born?") \n!pyramid <number>: COMING SOON: Makes an emoji pyramid, the maximum amount of emojis will be the number you specified. Max number allowed: 27 \n!say <phrase>: Says what you typed! \n!rr/!russianroulette: Plays a game of Russian roulette. 1 in 6 chambers.\n!catpicoftheday: BETA: Triggers a random Trevor pic\n!mindbleach/!eyebleach: Shows you a calming/cute photo (of Trevor).\n!howtrueis: How true is the info you requested?\n!tehurn/!nottehurn: tehurn.com and tehurn.com/not\n!trevporn: Gives you a (SFW) video of Trevor and his blankets \n!rolemarket: EXCLUSIVE TO THIS SRSS: Buy some colors! \n!info: Detailed information about the bot\'s hosting PC');}
  if (msg.content.toLowerCase() === '!info') {
 	//msg.channel.send(cpuStat.usagePercent(remb));
 	msg.channel.send(resource_embed());
@@ -163,6 +194,14 @@ if (msg.content.toLowerCase() === '!piggy') {
 			"./video/tehurn.webm"
 		  ]
 	  });
+  }
+  if (msg.content.toLowerCase() === '​<@467610653050404880>​') {
+     var s = new Discord.RichEmbed()
+    .setTitle("Pong!")
+    .setAuthor("Trevor", client.user.avatarURL)
+    .setColor(3447003)
+    .setImage("https://cdn.discordapp.com/attachments/429019393557659654/473787066044383234/trevmeme.png");
+    msg.channel.send(s);
   }
   if (msg.content.toLowerCase() === '!nottehurn') {
 	  msg.channel.send('Not teh urn... :(', 
@@ -239,12 +278,45 @@ if (msg.content.toLowerCase().startsWith ('!howtrueis')) {
 	var speak = msg.content.substring(11,msg.content.length);
 	console.log(speak);
 	msg.channel.send(speak + ' is' + ' ' + rn + ' % likely to be true.');}
+if (msg.content.toLowerCase() === '!rr' | msg.content.toLowerCase() === '!russianroulette') {
+    var rr = Math.floor(Math.random()*7); 
+    console.log(rr);
+    if (rr === 2)
+    {msg.channel.send('**Died! Oof.** :x:');}
+    else
+    {msg.channel.send('**Survived!** :white_check_mark:');}}
+if (msg.content.toLowerCase() === '!rolemarket') {
+     if (msg.guild.id === '429018214274564126')
+{msg.channel.send('Want to buy some colors? You are in the right place! \nVeterans discounts: \n50% if you were there during SRRH. Add 10% if you were there during when it was just made. \n40% if you joined when SRSS was just made \nIf you apply to multiple of these, the highest one will be applied. You will be paid back the number of your discount if it applies to you. \nEach color costs 250 tatsumaki credits! \nColors: \nInvisible \nTurquoise \nLight Green \nBlue \nPink \nRed \nYellow \nDark Turquoise \nGreen \nNavy Blue \nUltra Blue \n Once you have chosen your color, react with an emoji that corresponds with the color, if not available, just specify the color. Once you have done this, do t!credits @sullyrox 250.');}
+  else
+{msg.channel.send(error_embed("A server not in the list of servers with the role market attempted to run this bot command."));}}
+	if (msg.content.toLowerCase() === '!trevporn') {var pron_b = Math.floor(Math.random()*2);
+      if (pron_b === 0 | pron_b === 2)
+	  {rn_video = "blankies_2.webm";}
+      if (pron_b === 1)
+	  {rn_video = "blankets.webm";}
+      msg.channel.send('Enjoy your blanket porn...', 
+      {
+          files: [
+            "./video/" + rn_video
+          ]
+      });
+  }
+  if (msg.content.toLowerCase() === '!mlm') {
+    var s = new Discord.RichEmbed()
+    .setTitle("After selling $50,000,000 of makeup and losing all my friends, I'm SO happy to have bought the car of my dreams!")
+    .setAuthor("Trevor", client.user.avatarURL)
+    .setColor(3447003)
+    .setImage("https://cdn.discordapp.com/attachments/429018214274564128/484627656957100049/oL5oEG3u4eOYxxGjALF7fo7o0pxQ2thHn3GdUzaaaYM.png");
+    msg.channel.send(s);
+    }
+
 if (msg.content.toLowerCase() === '!catpicoftheday') {
 	function randomURL() {
-    var t = Math.floor(Math.random()*10); 
+    var t = Math.floor(Math.random()*15); 
     console.log(t);
     var urls = "https://zmeykolbasnik.github.io/";
-    var urle = ["party%20trev.jpg","Trevor%20on%20desk.jpg","snuggly%20trev.jpg","trevwut.JPG","TrevSleep.jpg","Full%20Resolution%20Trevor%20Yawn.jpg", "crazy%20trev.jpg","baguette%20trev.png","snuggly%20trev.jpg","%5B8K%5D%20Trevor.JPG"]
+    var urle = ["party%20trev.jpg","Trevor%20on%20desk.jpg","snuggly%20trev.jpg","trevwut.JPG","TrevSleep.jpg","Full%20Resolution%20Trevor%20Yawn.jpg", "crazy%20trev.jpg","baguette%20trev.png","snuggly%20trev.jpg","%5B8K%5D%20Trevor.JPG", "sup%20ladies.png", "mindbleach%5Fsticker.jpg", "TwebbieRage.JPG", "trev%5Fsucc.jpg", "trev%5Fstatue.JPG"]
     return urls + urle[t];
 }
 function imageEmbed(title, url) {
